58b1cc37406a4ab22e4a70afbc25a95ff1810d25
Andrej Pedchenko <graduadu@gmail.com>
2021-12-12 20:06:54 +0200

    Moved handlers to separate file.

--- README.md
+++ README.md
@@ -6,3 +6,11 @@
 
 ## External package list
 * [julienschmidt/httprouter](https://github.com/julienschmidt/httprouter)
+
+## API
+* GET `/users` -- user list --`200, 404, 500`
+* GET `/users/:id` -- user by id --`200, 404, 500`
+* POST `/users` -- create user --`201, 500`
+* PUT `/users/:id` -- full update user data --`204/200, 404, 500`
+* PATCH `/users/:id` -- partially update user data --`204/200, 404, 500`
+* DELETE `/users/:id` -- delete user by id -- `204, 404, 500`
---
+++ internal/user/handler.go
@@ -0,0 +1,72 @@
+package user
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+const (
+	usersURL = "/users"
+	userURL  = "/users/:uuid"
+)
+
+type handler struct {
+}
+
+// Register realise Handler interface (from "internal/handlers/handler.go" file)
+func (h *handler) Register(router *httprouter.Router) {
+	router.GET(usersURL, h.GetList)
+	router.GET(userURL, h.GetUserByUUID)
+	router.POST(usersURL, h.CreateUser)
+	router.PUT(userURL, h.UpdateUser)
+	router.PATCH(userURL, h.PartiallyUpdateUser)
+	router.DELETE(userURL, h.DeleteUserByUUID)
+}
+
+// GetList get user list
+func (h *handler) GetList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	_, err := w.Write([]byte("This is list of users"))
+	if err != nil {
+		panic(err)
+	}
+}
+
+// GetUserByUUID get user by uuid
+func (h *handler) GetUserByUUID(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	_, err := w.Write([]byte("Get user by uuid"))
+	if err != nil {
+		panic(err)
+	}
+}
+
+// CreateUser create new user
+func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	_, err := w.Write([]byte("Create new user"))
+	if err != nil {
+		panic(err)
+	}
+}
+
+// UpdateUser fully update user data
+func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	_, err := w.Write([]byte("Fully update user data"))
+	if err != nil {
+		panic(err)
+	}
+}
+
+// PartiallyUpdateUser partially update user data
+func (h *handler) PartiallyUpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	_, err := w.Write([]byte("Partially update user data"))
+	if err != nil {
+		panic(err)
+	}
+}
+
+// DeleteUserByUUID delete user by uuid
+func (h *handler) DeleteUserByUUID(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	_, err := w.Write([]byte("Delete user by uuid"))
+	if err != nil {
+		panic(err)
+	}
+}
