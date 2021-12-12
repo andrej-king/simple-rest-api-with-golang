fd4a7827ad39a7dc660c39306451cd6b062d110c
Andrej Pedchenko <graduadu@gmail.com>
2021-12-12 21:12:43 +0200

    Added logger settings.

---
+++ pkg/logging/logging.go
@@ -0,0 +1,82 @@
+package logging
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"io"
+	"os"
+	"path"
+	"runtime"
+)
+
+type writerHook struct {
+	Writer    []io.Writer
+	LogLevels []logrus.Level
+}
+
+func (hook *writerHook) Fire(entry *logrus.Entry) error {
+	line, err := entry.String()
+	if err != nil {
+		return err
+	}
+
+	for _, w := range hook.Writer {
+		_, err := w.Write([]byte(line))
+		if err != nil {
+			return err
+		}
+	}
+	return err
+}
+
+func (hook *writerHook) Levels() []logrus.Level {
+	return hook.LogLevels
+}
+
+var entry *logrus.Entry
+
+type Logger struct {
+	*logrus.Entry
+}
+
+// GetLogger get logger
+func GetLogger() Logger {
+	return Logger{entry}
+}
+
+// GetLoggerWithField logger with field
+func (logger *Logger) GetLoggerWithField(key string, value interface{}) Logger {
+	return Logger{logrus.WithField(key, value)}
+}
+
+func Init() {
+	logger := logrus.New()
+	logger.SetReportCaller(true)
+	logger.Formatter = &logrus.TextFormatter{
+		CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
+			filename := path.Base(frame.File)
+			return fmt.Sprintf("%s()", frame.Function), fmt.Sprintf("%s:%d", filename, frame.Line)
+		},
+		DisableColors: false,
+		FullTimestamp: true,
+	}
+
+	err := os.Mkdir("logs", 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	allFile, err := os.OpenFile("logs/all.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	logrus.SetOutput(io.Discard)
+
+	logger.AddHook(&writerHook{
+		Writer:    []io.Writer{allFile, os.Stdout},
+		LogLevels: logrus.AllLevels,
+	})
+
+	logger.SetLevel(logrus.TraceLevel)
+}
